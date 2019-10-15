d987884a604877143692cbb12d68474ed69b3e79
sandreandreandre <56262227+sandreandreandre@users.noreply.github.com>
2019-10-14 23:04:04 -0700

    Add files via upload

---
+++ JohnDoesBroswer/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace JohnDoesBroswer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblEncrypted = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDecrypted = new System.Windows.Forms.Label();
+            this.getStock = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.getPrice = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(32, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1587, 428);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(32, 481);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(1410, 33);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "http://";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(1494, 460);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(95, 68);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.BtnGo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 574);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "pleace enter a string for encryption";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(375, 571);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(278, 33);
+            this.txtInput.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(711, 555);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 58);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 636);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "The encrypt string look like this";
+            // 
+            // lblEncrypted
+            // 
+            this.lblEncrypted.AutoSize = true;
+            this.lblEncrypted.Location = new System.Drawing.Point(371, 636);
+            this.lblEncrypted.Name = "lblEncrypted";
+            this.lblEncrypted.Size = new System.Drawing.Size(112, 21);
+            this.lblEncrypted.TabIndex = 7;
+            this.lblEncrypted.Text = "lblEncrypted";
+            this.lblEncrypted.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 698);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(291, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Check if decrypted string is correct";
+            // 
+            // lblDecrypted
+            // 
+            this.lblDecrypted.AutoSize = true;
+            this.lblDecrypted.Location = new System.Drawing.Point(371, 698);
+            this.lblDecrypted.Name = "lblDecrypted";
+            this.lblDecrypted.Size = new System.Drawing.Size(113, 21);
+            this.lblDecrypted.TabIndex = 9;
+            this.lblDecrypted.Text = "lblDecrypted";
+            // 
+            // getStock
+            // 
+            this.getStock.Location = new System.Drawing.Point(1165, 555);
+            this.getStock.Name = "getStock";
+            this.getStock.Size = new System.Drawing.Size(277, 33);
+            this.getStock.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1165, 621);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(277, 61);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "StockPrice";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // getPrice
+            // 
+            this.getPrice.AutoSize = true;
+            this.getPrice.Location = new System.Drawing.Point(1161, 708);
+            this.getPrice.Name = "getPrice";
+            this.getPrice.Size = new System.Drawing.Size(75, 21);
+            this.getPrice.TabIndex = 12;
+            this.getPrice.Text = "getPrice";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1694, 754);
+            this.Controls.Add(this.getPrice);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.getStock);
+            this.Controls.Add(this.lblDecrypted);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblEncrypted);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "John Does Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblEncrypted;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDecrypted;
+        private System.Windows.Forms.TextBox getStock;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label getPrice;
+    }
+}
+
---
+++ JohnDoesBroswer/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JohnDoesBroswer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGo_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(txtUrl.Text);
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            EncryptService.ServiceClient myClient = new EncryptService.ServiceClient();
+            try { lblEncrypted.Text = myClient.Encrypt(txtInput.Text); }
+            catch (Exception ec) { lblEncrypted.Text = ec.Message.ToString(); }
+            try { lblDecrypted.Text = myClient.Decrypt(lblEncrypted.Text); }
+            catch (Exception dc) { lblDecrypted.Text = dc.Message.ToString(); }
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            StockService.ServiceClient myclient = new StockService.ServiceClient();
+            string stockPrice = myclient.getStockquote(getStock.Text);
+            getPrice.Text = stockPrice;
+        }
+    }
+}
